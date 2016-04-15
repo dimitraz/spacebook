c2347e09f0ebdb06c44584252347e2b6023751be
Gianluca <jamaica.zuccarelli@gmail.com>
2016-04-15 12:14:04 +0100

    First commit

---
+++ app/controllers/Blog.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.util.List;
+
+import models.Message;
+import models.Post;
+import models.User;
+import play.Logger;
+import play.mvc.Controller;
+
+public class Blog  extends Controller
+{
+  public static void index()
+  {
+    User user = Accounts.getLoggedInUser();
+    render(user);
+  }
+  
+  public static void newPost(String title, String content)
+  {
+    User user = Accounts.getLoggedInUser();
+    
+    Post post = new Post (title, content);
+    post.save();
+    user.posts.add(post);
+    user.save();
+    
+    Logger.info ("title:" + title + " content:" + content);
+    index();
+  }
+  
+  public static void deletePost(Long postid) {
+	User user = Accounts.getLoggedInUser();
+	
+	Post post = Post.findById(postid);
+	user.posts.remove(post);
+	
+	user.save();
+	post.delete();
+	
+	index();
+  }
+  
+}
---
+++ app/controllers/Profile.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import play.*;
+import play.db.jpa.Blob;
+import play.mvc.*;
+
+import java.io.File;
+import java.util.*;
+
+import models.*;
+
+public class Profile extends Controller
+{
+  public static void index()
+  {
+    User user = Accounts.getLoggedInUser();
+    render(user);
+  }
+  
+  public static void changeStatus(String statusText)
+  {
+    User user = Accounts.getLoggedInUser();
+    user.statusText = statusText;
+    user.save();
+    Logger.info("Status changed to " + statusText);
+    index();
+  }
+  
+	public static void getPicture(Long id) {
+		String[] images = {"public/images/avatar-1.jpg", "public/images/avatar-2.jpg", "public/images/avatar-3.jpg"};
+		int random = new Random().nextInt(images.length);
+			
+	    User user = User.findById(id);
+	    Blob picture = user.profilePicture;
+	    
+	    if (picture.exists())
+	    {
+	      response.setContentTypeIfNotSet(picture.type());
+	      renderBinary(picture.get());
+	    } else {
+	    	renderBinary(new File(images[random]));
+	    }
+	}
+  
+  public static void uploadPicture(Long id, Blob picture)
+  {
+    User user = User.findById(id);
+    user.profilePicture = picture;
+    user.save();
+    Logger.info("saving picture");
+    index();
+  }  
+  
+  public static void getThumbnail(Long id) 
+  {
+    User user = User.findById(id);
+    Blob picture = user.thumbnailPicture;
+    if (picture.exists())
+    {
+      response.setContentTypeIfNotSet(picture.type());
+      renderBinary(picture.get());
+    }
+  }
+
+  public static void uploadThumbnail(Long id, Blob picture)
+  {
+    User user = User.findById(id);
+    user.thumbnailPicture = picture;
+    user.save();
+    index();
+  } 
+}
