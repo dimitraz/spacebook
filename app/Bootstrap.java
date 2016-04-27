277416a20ce28b7a651830a7c07aa57dd86af30c
Dimitra <dimitrazuccarelli@gmail.com>
2016-04-27 21:48:14 +0100

    Bootstrap

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -18,6 +18,9 @@ public class Bootstrap extends Job
 	  Fixtures.deleteDatabase();
       Fixtures.loadModels("data.yml");
       
+      List<User> users = User.findAll();
+      Logger.info("Blah" + users.get(2).posts);
+      
       String photoName1 = "lisa.jpg";
       String photoName2 = "lisacover.jpg";
       Blob blob1 = new Blob();
@@ -29,10 +32,14 @@ public class Bootstrap extends Job
       lisa.coverPicture = blob2;
       lisa.save();
       
+      String photoName3 = "marge.png";
       String photoName4 = "margecover.jpg";
+      Blob blob3 = new Blob();
       Blob blob4 = new Blob();
+      blob3.set(new FileInputStream(photoName3), MimeTypes.getContentType(photoName3));
       blob4.set(new FileInputStream(photoName4), MimeTypes.getContentType(photoName4));
       User marge = User.findByEmail("marge@simpson.com");
+      marge.profilePicture = blob3;
       marge.coverPicture = blob4;
       marge.save();  
       
@@ -47,12 +54,26 @@ public class Bootstrap extends Job
       bart.coverPicture = blob6;
       bart.save();  
       
-      
+      String photoName7 = "maggie.png";
       String photoName8 = "maggiecover.jpg";
+      Blob blob7 = new Blob();
       Blob blob8 = new Blob();
+      blob7.set(new FileInputStream(photoName7), MimeTypes.getContentType(photoName7));
       blob8.set(new FileInputStream(photoName8), MimeTypes.getContentType(photoName8));
       User maggie = User.findByEmail("maggie@simpson.com");
+      maggie.profilePicture = blob7;
       maggie.coverPicture = blob8;
       maggie.save();  
+      
+      String photoName9 = "homer.png";
+      String photoName10 = "homercover.jpg";
+      Blob blob9 = new Blob();
+      Blob blob10 = new Blob();
+      blob9.set(new FileInputStream(photoName9), MimeTypes.getContentType(photoName9));
+      blob10.set(new FileInputStream(photoName10), MimeTypes.getContentType(photoName10));
+      User homer = User.findByEmail("homer@simpson.com");
+      homer.profilePicture = blob9;
+      homer.coverPicture = blob10;
+      homer.save(); 
   }
 }
